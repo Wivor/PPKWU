bf903f89009746bea40a6cb022e770be924ac56d
Wiktor <wiktor756@gmail.com>
2019-10-22 16:59:14 +0200

    zad2 - string info

--- src/main/java/main/Controller.java
+++ src/main/java/main/Controller.java
@@ -1,15 +1,22 @@
 package main;
 
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
 
 @RestController
 public class Controller {
-    Service service = new Service();
+    private Service service = new Service();
 
-    @RequestMapping("/rev")
-    public String greeting(@RequestParam String string) {
-        return service.greeting(string);
+    @RequestMapping("/rev/{string}")
+    public String rev(@PathVariable String string) {
+        return service.reverse(string);
+    }
+
+    @RequestMapping("/string/{string}")
+    public Map string(@PathVariable String string) {
+        return service.stringInfo(string);
     }
 }
--- src/main/java/main/Service.java
+++ src/main/java/main/Service.java
@@ -1,7 +1,37 @@
 package main;
 
-public class Service {
-    String greeting (String string){
+import java.util.Map;
+import java.util.TreeMap;
+
+class Service {
+    String reverse(String string){
         return new StringBuilder(string).reverse().toString();
+    }
+
+    Map stringInfo(String string){
+        TreeMap<String, Integer> infoMap = new TreeMap<>();
+        int upperCase = 0;
+        int lowerCase= 0;
+        int numbers = 0;
+        int specials = 0;
+
+
+        for (char ch: string.toCharArray()) {
+            if (Character.isUpperCase(ch))
+                upperCase += 1;
+            else if (Character.isLowerCase(ch))
+                lowerCase += 1;
+            else if (Character.isDigit(ch))
+                numbers += 1;
+            else
+                specials += 1;
+        }
+
+        infoMap.put("Upper Case Letters", upperCase);
+        infoMap.put("Lower Case Letters", lowerCase);
+        infoMap.put("Numbers", numbers);
+        infoMap.put("Special Signs", specials);
+
+        return infoMap;
     }
 }
